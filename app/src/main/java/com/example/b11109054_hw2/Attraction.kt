7c18d6be9b702f9bf23351604b9bdbaa906be7b5
gut2003817 <132485552+gut2003817@users.noreply.github.com>
2024-05-15 15:24:21 +0800

    add Attraction.kt

---
+++ app/src/main/java/com/example/b11109054_hw2/Attraction.kt
@@ -0,0 +1,8 @@
+package com.example.b11109054_hw2
+
+data class Attraction(
+    val name: String,
+    val description: String,
+    val address: String,
+    val imageResourceID: Int // 整數類型的圖片資源 ID
+)
