75fa40dfc25eaf6d0db3b2e97425c29fba6cbf75
gut2003817 <132485552+gut2003817@users.noreply.github.com>
2024-05-16 17:48:22 +0800

    add Mycolors

---
+++ app/src/main/java/com/example/b11109054_hw2/Mycolors.kt
@@ -0,0 +1,10 @@
+package com.example.b11109054_hw2
+
+import androidx.compose.ui.graphics.Color
+
+object MyColors {
+    val Green = Color(0xFFD4D4AA)
+    val frontpageTextBackground = Color(0xFFEBF5DF)
+    val frontpageAttractionsName = Color(0xFFBAD4AA)
+    val backgroundColor = Color(0xFFf4fad7)
+}
